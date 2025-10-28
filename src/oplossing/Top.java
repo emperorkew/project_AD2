1d7f30a5b8a71123cca4539a50daf85b0afed4ab
remco marien <remco.marien@ugent.be>
2025-10-28 15:48:16 +0100

    Test push

---
+++ src/oplossing/Top.java
@@ -0,0 +1,39 @@
+package oplossing;
+
+import opgave.Node;
+
+public class Top<E extends Comparable<E>> implements Node<E> {
+
+    private E value;
+    private Top<E> left;
+    private Top<E> right;
+
+    public Top(E value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+
+    @Override
+    public E getValue() {
+        return this.value;
+    }
+
+    @Override
+    public Node<E> getLeft() {
+        return this.left;
+    }
+
+    @Override
+    public Node<E> getRight() {
+        return this.right;
+    }
+
+    public void setLeft(Top<E> left) {
+        this.left = left;
+    }
+
+    public void setRight(Top<E> right) {
+        this.right = right;
+    }
+}
