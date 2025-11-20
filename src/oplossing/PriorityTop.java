01f3b90ca4421419569d0f53781768e67e105f40
remco marien <remco.marien@ugent.be>
2025-11-20 09:25:10 +0100

    PriorityTop geimplementeerd

--- src/oplossing/PriorityTop.java
+++ src/oplossing/PriorityTop.java
@@ -4,23 +4,52 @@ import opgave.PriorityNode;
 
 public class PriorityTop<E extends Comparable<E>> implements PriorityNode<E> {
 
-    @Override
-    public long getPriority() {
-        return 0;
+    private E value;
+    private long priority;
+    private PriorityTop<E> left;
+    private PriorityTop<E> right;
+
+    public PriorityTop(E value, long priority) {
+        this.value = value;
+        this.priority = priority;
+        this.left = null;
+        this.right = null;
     }
 
     @Override
     public E getValue() {
-        return null;
+        return value;
+    }
+
+    @Override
+    public long getPriority() {
+        return priority;
     }
 
     @Override
     public PriorityNode<E> getLeft() {
-        return null;
+        return left;
     }
 
     @Override
     public PriorityNode<E> getRight() {
-        return null;
+        return right;
+    }
+
+    // Setters for tree manipulation
+    public void setValue(E value) {
+        this.value = value;
+    }
+
+    public void setPriority(long priority) {
+        this.priority = priority;
+    }
+
+    public void setLeft(PriorityTop<E> left) {
+        this.left = left;
+    }
+
+    public void setRight(PriorityTop<E> right) {
+        this.right = right;
     }
 }
